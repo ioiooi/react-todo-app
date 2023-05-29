5dcf4f56ddf63fe6baf175dc7fc4392cecac6678
Eugen Mayer <eugen.mayer2@gmail.com>
2023-05-29 12:18:29 +0200

    ChatGPT todoapp with bulma

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # React Todo
 
 Using [react-starter](https://github.com/ioiooi/react-starter) as base.
+
+ChatGPT prompt: `create a SPA todo app using react and bulma.io`
--- src/App.js
+++ src/App.js
@@ -1,35 +1,66 @@
-import React, { useEffect, useState, useRef } from "react";
+import "bulma/css/bulma.css";
+import React, { useState } from "react";
 
-const App = () => {
-  const [count, setCount] = useState(0);
-  const countRef = useRef(count);
+const TodoApp = () => {
+  const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState("");
 
-  // Within the first useEffect hook, whenever the count value changes, the ref value
-  // countRef.current is updated to match the latest count value. This ensures that the ref
-  // always holds the correct and up-to-date count value.
-  useEffect(() => {
-    countRef.current = count;
-  }, [count]);
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
 
-  // Then, in the second useEffect hook, the countRef.current value (which represents
-  // the latest count value) is used to increment and update the count state within the
-  // setInterval callback.
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      setCount(countRef.current + 1);
-    }, 1000);
+  const handleAddTodo = () => {
+    if (inputValue) {
+      const newTodo = {
+        id: todos.length + 1,
+        text: inputValue,
+      };
+      setTodos([...todos, newTodo]);
+      setInputValue("");
+    }
+  };
 
-    return () => {
-      clearInterval(intervalId);
-    };
-  }, []);
+  const handleRemoveTodo = (id) => {
+    const updatedTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(updatedTodos);
+  };
 
   return (
-    <div>
-      <p>Why, hello there.</p>
-      <p id="counter">{count}</p>
+    <div className="container">
+      <h1 className="title">Todo App</h1>
+      <div className="field has-addons">
+        <div className="control">
+          <input
+            className="input"
+            type="text"
+            placeholder="Enter a new todo"
+            value={inputValue}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="control">
+          <button className="button is-primary" onClick={handleAddTodo}>
+            Add
+          </button>
+        </div>
+      </div>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id} className="notification">
+            <div className="level">
+              <div className="level-left">{todo.text}</div>
+              <div className="level-right">
+                <button
+                  className="delete"
+                  onClick={() => handleRemoveTodo(todo.id)}
+                />
+              </div>
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
 
-export default App;
+export default TodoApp;
